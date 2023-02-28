da474ae10a720485339b0e27e497a0b33fcd7b02
Andrew J Sides <56322348+AndrewJSides@users.noreply.github.com>
2023-02-27 22:39:32 -0800

    seo

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Head from "next/head";
+
 import About from "../components/About";
 const AboutPage = () => {
-  return <About />;
+  return (
+    <>
+      <Head>
+        <title>Andrew Sides</title>
+        <meta
+          name="description"
+          content="Currently a Software Engineer, this is my personal webpage."
+        />
+      </Head>
+      <About />
+    </>
+  );
 };
 
 export default AboutPage;
